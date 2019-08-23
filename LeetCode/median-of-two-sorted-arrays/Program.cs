9781dc0b86b916fef30e90ec3899ca5fe2457daf
JeromePeng <JeromePen@witsamerica.com>
2019-08-23 17:47:07 +0800

    1.0

--- LeetCode/median-of-two-sorted-arrays/Program.cs
+++ LeetCode/median-of-two-sorted-arrays/Program.cs
@@ -11,20 +11,30 @@ namespace median_of_two_sorted_arrays
         static void Main(string[] args)
         {
             int[] nums1 = { 1, 2, 3, 5, 6, 8, 9, 12 };
-            int[] nums2 = { 4, 6, 7, 13, 16 };
+            int[] nums2 = { 4, 7, 13, 16, 18 };
             Console.WriteLine(FindMedianSortedArrays(nums1, nums2));
         }
 
         static double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
             int result = 0;
-            if (nums1[nums1.Length - 1] < nums2[0])
+            int m = nums1.Length;
+            int n = nums2.Length;
+            int middle = (m + n) / 2;
+            if (nums1[m - 1] < nums2[0])
             {
-                result = (nums1[nums1.Length - 1] + nums2[0]) / 2;
+                result = (nums1[m - 1] + nums2[0]) / 2;
             }
             else
             {
+                if (nums1[middle] < nums2[0])
+                {
+                    result = nums1[middle];
+                }
+                else
+                {
 
+                }
             }
             return result;
         }
